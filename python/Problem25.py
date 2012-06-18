7ac35d99a60fe822d77af28daf5aac933ad3a811
Ryosuke Ito <rito.0305@gmail.com>
2012-06-18 23:34:16 +0900

    update

---
+++ python/Problem25.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+A unit fraction contains 1 in the numerator. The decimal representation of the unit fractions with denominators 2 to 10 are given:
+
+1/2	= 	0.5
+1/3	= 	0.(3)
+1/4	= 	0.25
+1/5	= 	0.2
+1/6	= 	0.1(6)
+1/7	= 	0.(142857)
+1/8	= 	0.125
+1/9	= 	0.(1)
+1/10	= 	0.1
+Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be seen that 1/7 has a 6-digit recurring cycle.
+
+Find the value of d  1000 for which 1/d contains the longest recurring cycle in its decimal fraction part.
+'''
+def felm(num):
+    for d in range(1, num):
+        for l in range(1, num):
+            if (10 ** l) % d == 1: yield l, d; break
+
+if __name__ == '__main__':
+    res = dict(i for i in felm(1001))
+    print res[max([i for i in res.iterkeys()])]
