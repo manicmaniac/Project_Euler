b8a405593cbc004b00d8d533879ad4b797508a6e
Ryosuke Ito <rito.0305@gmail.com>
2012-06-28 20:00:09 +0900

    update

---
+++ python/Problem36.py
@@ -0,0 +1,23 @@
+#/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+
+Find the sum of all numbers, less than one million, which are palindromic in base 10 and base 2.
+
+(Please note that the palindromic number, in either base, may not include leading zeros.)
+'''
+
+def is_palindromic(n):
+    return str(n) == str(n)[::-1]
+
+def base2(n):
+    return int(format(n, 'b'))
+
+if __name__ == '__main__':
+    acc = 0
+    for i in range(1000000):
+        if is_palindromic(i) and is_palindromic(base2(i)):
+            acc += i
+    print acc
+
