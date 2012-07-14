e626b31a995856f11b5a1a83f5b41119d85d63ad
Ryosuke Ito <rito.0305@gmail.com>
2012-07-14 13:26:57 +0900

    update

---
+++ python/Problem25.py
@@ -0,0 +1,40 @@
+#/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+The Fibonacci sequence is defined by the recurrence relation:
+
+    F[n] = F[n−1] + F[n−2], where F[1] = 1 and F[2] = 1.
+
+Hence the first 12 terms will be:
+
+    F[1] = 1
+    F[2] = 1
+    F[3] = 2
+    F[4] = 3
+    F[5] = 5
+    F[6] = 8
+    F[7] = 13
+    F[8] = 21
+    F[9] = 34
+    F[10] = 55
+    F[11] = 89
+    F[12] = 144
+
+The 12th term, F[12], is the first term to contain three digits.
+
+What is the first term in the Fibonacci sequence to contain 1000 digits?
+'''
+
+def gen_fib():
+    s, t = 0, 1
+    index = 0
+    while True:
+        s, t = t, s + t
+        index += 1
+        yield index, s
+
+if __name__ == '__main__':
+    for i, j in gen_fib():
+        if len(str(j)) >= 1000:
+            print i
+            break
