9205faf1be018054f8245b0813d385b3003db9fa
Ryosuke Ito <rito.0305@gmail.com>
2012-07-14 17:35:39 +0900

    update

---
+++ scala/Problem9.scala
@@ -0,0 +1,25 @@
+/*
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+a^2 + b^2 = c^2
+
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc.
+*/
+
+object Problem9 {
+  def findPythagoreanTripret(sum:Long, m:Long=0, n:Long=0):List[Long] = m match {
+    case _ if(m*m-n*n + 2*m*n + m*m+n*n == sum) => {
+      List(m*m-n*n, 2*m*n, m*m+n*n)
+    }
+    case _ if(m <= n) => findPythagoreanTripret(sum, m+1, 0)
+    case _ if(m > n) => findPythagoreanTripret(sum, m, n+1)
+    case _ => null
+
+  }
+  def main(args:Array[String]) {
+    println(findPythagoreanTripret(1000).reduce(_*_))
+  }
+}
