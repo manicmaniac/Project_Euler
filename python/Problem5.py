d1ae236b7ee3ae1c779d2b099fcfe0ab7911ea22
Ryosuke Ito <rito.0305@gmail.com>
2012-07-25 00:31:17 +0900

    update

--- python/Problem5.py
+++ python/Problem5.py
@@ -4,17 +4,12 @@
 #2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 #What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-def FindDevisibleNumber(num):
-    import prime
-    print(prime.factorize(num))
+from operator import mul
+import prime
 
 if __name__ == '__main__':
-    import prime
-    res = []
-    ans = 1
+    res = 1
     for i in range(2, 11):
         lst = prime.factorize(i)
-        for j in range(len(lst)):
-            ans *= lst[j]
-        print(lst)
-    print ans
+        res *= reduce(mul, lst)
+    print res
