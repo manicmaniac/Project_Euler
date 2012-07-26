a54da545d6c72029c6e3661b03c50c2825b9e226
Ryosuke Ito <rito.0305@gmail.com>
2012-07-27 08:47:11 +0900

    update

---
+++ scala/Problem15.scala
@@ -0,0 +1,18 @@
+/*
+Starting in the top left corner of a 2×2 grid, there are 6 routes (without
+backtracking) to the bottom right corner.
+
+[p_015]
+
+How many routes are there through a 20×20 grid?
+*/
+
+object Problem15 {
+  def fact(n:BigInt) = (BigInt(1) to n).product
+  def c(m:BigInt, n:BigInt) = {
+    (fact(m) / fact(n)) / fact(n)
+  }
+  def main(args:Array[String]) {
+    println(c(40, 20))
+  }
+}
