50920e9f5d24cab2010d2b965ff9bbf988c6f279
Ryosuke Ito <rito.0305@gmail.com>
2012-07-26 20:36:24 +0900

    update

---
+++ scala/Problem20.scala
@@ -0,0 +1,18 @@
+/*
+n ! means  n    ( n    1)   ...   3   2   1
+For example, 10! = 10   9   ...   3   2   1 = 3628800, and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+Find the sum of the digits in the number 100!
+*/
+
+object Problem20 {
+  def fact(n:Int) = {
+    (1 to n).map(p => BigInt(p)).product
+  }
+  def sumDigits(n:BigInt) = {
+    n.toString.toList.map(_.toString.toInt).sum
+  }
+  def main(args:Array[String]) {
+    println(sumDigits(fact(100)))
+  }
+}
+
