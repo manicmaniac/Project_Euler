ac51512645522e6649423012008fb88e7ba8a590
Ryosuke Ito <rito.0305@gmail.com>
2012-07-27 21:57:36 +0900

    update

---
+++ python/Problem52.py
@@ -0,0 +1,21 @@
+#/usr/bin/env python
+# -*- coding:utf-8 -*-
+'''
+It can be seen that the number, 125874, and its double, 251748, contain exactly
+the same digits, but in a different order.
+
+Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x,
+contain the same digits.
+'''
+
+def digits(n):
+    return sorted(int(i) for i in str(n))
+
+def has_same_digits(n):
+    res = digits(n) == digits(n*2) == digits(n*3) == digits(n*4) == digits(n*5) == digits(n*6)
+    return res
+
+if __name__ == '__main__':
+    res = 1
+    while not has_same_digits(res): res += 1
+    print res
