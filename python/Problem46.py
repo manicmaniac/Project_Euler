4fdd4ff9141b3c1c893fec902862e57094c6cb10
Ryosuke Ito <rito.0305@gmail.com>
2012-07-27 21:16:20 +0900

    update

--- python/Problem46.py
+++ python/Problem46.py
@@ -14,4 +14,29 @@ It turns out that the conjecture was false.
 
 What is the smallest odd composite that cannot be written as the sum of a prime and twice a square?
 '''
+from prime import genprime, isprime
+from itertools import count
+
+def gen_square(max):
+    res = i = 0
+    while res <= max:
+        i += 1
+        res = 2 * i ** 2
+        yield res
+
+
+def is_composite_of_prime_and_square(n):
+    if isprime(n): return True
+    for i in genprime(n):
+        for j in gen_square(n):
+            if i + j == n:
+                return True
+    return False
+
+
+if __name__ == '__main__':
+    for i in count(5, 2):
+        if not is_composite_of_prime_and_square(i):
+            print i
+            break
 
