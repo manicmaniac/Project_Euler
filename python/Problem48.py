f743db1bb3aca7cc24fbd64a5464cc89dbe20242
Ryosuke Ito <rito.0305@gmail.com>
2012-07-27 12:57:52 +0900

    update

--- python/Problem48.py
+++ python/Problem48.py
@@ -3,12 +3,5 @@
 #The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
 #Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
 
-def f(max):
-    res = 0
-    for i in range(1, max + 1):
-        res += pow(i, i)
-    return str(res)[-10:]
-
-if __name__ == '__main__':
-    print f(1000)
+print str(sum(i**i for i in range(1, 1001)))[-10:]
 
