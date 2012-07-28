69771959e84d5451dc45574723a0e59d2ce51fc8
Ryosuke Ito <rito.0305@gmail.com>
2012-07-28 23:00:58 +0900

    update

--- scala/Problem17.scala
+++ scala/Problem17.scala
@@ -14,45 +14,24 @@ British usage.
 
 object Problem17 {
   val dict = Map(
-    0 -> "",
-    1 -> "one",
-    2 -> "two",
-    3 -> "three",
-    4 -> "four",
-    5 -> "five",
-    6 -> "six",
-    7 -> "seven",
-    8 -> "eight",
-    9 -> "nine",
-    10 -> "ten",
-    11 -> "eleven",
-    12 -> "twelve",
-    13 -> "thirteen",
-    14 -> "fourteen",
-    15 -> "fifteen",
-    16 -> "sixteen",
-    17 -> "seventeen",
-    18 -> "eighteen",
-    19 -> "nineteen",
-    20 -> "twenty",
-    30 -> "thirty",
-    40 -> "forty",
-    50 -> "fifty",
-    60 -> "sixty",
-    70 -> "seventy",
-    80 -> "eighty",
-    90 -> "ninety",
-    100 -> "handred",
-    1000 -> "onethousand"
+    0 -> "", 1 -> "one", 2 -> "two", 3 -> "three", 4 -> "four", 5 -> "five",
+    6 -> "six", 7 -> "seven", 8 -> "eight", 9 -> "nine", 10 -> "ten",
+    11 -> "eleven", 12 -> "twelve", 13 -> "thirteen", 14 -> "fourteen", 15 -> "fifteen",
+    16 -> "sixteen", 17 -> "seventeen", 18 -> "eighteen", 19 -> "nineteen", 20 -> "twenty",
+    30 -> "thirty", 40 -> "forty", 50 -> "fifty", 60 -> "sixty", 70 -> "seventy",
+    80 -> "eighty", 90 -> "ninety", 100 -> "handred", 1000 -> "onethousand"
   )
-  def numToEn(n:Int):String = n match {
+
+  def numToEn(n:Int, res:String=""):String = n match {
+    case 0 => res
     case 1000 => dict(n)
-    case _ if(n<=20) => dict(n)
-    case _ if(n>20 && n<100) => dict(n.toString.take(1).toInt * 10) + dict(n % 10)
-    case _ if(n>=100 && n<1000) => dict(n / 100) + dict(100) + "and" +
-    dict(n % 100 - n % 100 % 10) + dict(n % 100 % 10)
+    case _ if(n%100==0) => res + dict(n/100) + dict(100)
+    case _ if(n<=20) => res + dict(n)
+    case _ if(n>20 && n<100) => numToEn(n%10, res + dict(n-n%10))
+    case _ if(n>=100) => numToEn(n%100, res + dict(n/100) + dict(100) + "and")
   }
+
   def main(args:Array[String]) {
-    println(((1 to 1000).map(numToEn).mkString).length - 1)
+    println(((1 to 1000).map(numToEn(_, "")).mkString).length)
   }
 }
