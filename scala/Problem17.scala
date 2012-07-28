169ec239a1e806e36e6a6adcaabe4682a585ae99
Ryosuke Ito <rito.0305@gmail.com>
2012-07-28 22:38:31 +0900

    update

---
+++ scala/Problem17.scala
@@ -0,0 +1,58 @@
+/*
+If the numbers 1 to 5 are written out in words: one, two, three, four, five,
+then there are 3 + 3 + 5 + 4 + 4 = 19 letters used in total.
+
+If all the numbers from 1 to 1000 (one thousand) inclusive were written out in
+words, how many letters would be used?
+
+
+NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and
+forty-two) contains 23 letters and 115 (one hundred and fifteen) contains 20
+letters. The use of "and" when writing out numbers is in compliance with
+British usage.
+*/
+
+object Problem17 {
+  val dict = Map(
+    0 -> "",
+    1 -> "one",
+    2 -> "two",
+    3 -> "three",
+    4 -> "four",
+    5 -> "five",
+    6 -> "six",
+    7 -> "seven",
+    8 -> "eight",
+    9 -> "nine",
+    10 -> "ten",
+    11 -> "eleven",
+    12 -> "twelve",
+    13 -> "thirteen",
+    14 -> "fourteen",
+    15 -> "fifteen",
+    16 -> "sixteen",
+    17 -> "seventeen",
+    18 -> "eighteen",
+    19 -> "nineteen",
+    20 -> "twenty",
+    30 -> "thirty",
+    40 -> "forty",
+    50 -> "fifty",
+    60 -> "sixty",
+    70 -> "seventy",
+    80 -> "eighty",
+    90 -> "ninety",
+    100 -> "handred",
+    1000 -> "onethousand"
+  )
+  def numToEn(n:Int):String = n match {
+    case 1000 => dict(n)
+    case _ if(n<=20) => dict(n)
+    case _ if(n>20 && n<100) => dict(n.toString.take(1).toInt * 10) + dict(n % 10)
+    case _ if(n>=100 && n<1000) => dict(n / 100) + dict(100) + "and" +
+    dict(n % 100 - n % 100 % 10) + dict(n % 100 % 10)
+  }
+  def main(args:Array[String]) {
+    println(((1 to 1000).map(numToEn).mkString).length - 1)
+  }
+}
