1f68dd6b4762f00e16eea05b35e59aba2a80af8d
Ryosuke Ito <rito.0305@gmail.com>
2012-07-28 18:07:30 +0900

    update

--- java/Problem1.java
+++ java/Problem1.java
@@ -4,7 +4,7 @@ get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
-class Project1 {
+class Problem1 {
 	public static int fizzbuzz() {
 		int acc = 0;
 		for(int i=0; i<1000; i++) {
