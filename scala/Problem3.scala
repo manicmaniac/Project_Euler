dc9319fd570b0d2e83730626c7614cc5baa6f02d
Ryosuke Ito <rito.0305@gmail.com>
2012-07-30 01:21:15 +0900

    update

--- scala/Problem3.scala
+++ scala/Problem3.scala
@@ -6,11 +6,11 @@ What is the largest prime factor of the number 600851475143 ?
 object Problem3 {
   def factorize(n:Long, d:Long=2, res:List[Long]=Nil):List[Long] = n match {
     case `d` => d::res reverse
-    case _ if(n % d == 0) => factorize(n/d, d, d::res)
+    case _ if(n%d==0) => factorize(n/d, d, d::res)
     case _ => factorize(n, d+1, res)
   }
   def main(args:Array[String]) {
-    println(factorize(600851475143L).max)
+    print(factorize(600851475143L) max)
   }
 }
 
