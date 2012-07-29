98f3743752503c765252d2ab4673f00a336ffea3
Ryosuke Ito <rito.0305@gmail.com>
2012-07-30 01:35:33 +0900

    update

--- scala/Problem4.scala
+++ scala/Problem4.scala
@@ -5,15 +5,7 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 */
 
 object Problem4 {
-  def isPalindrome(n:Int) = {
-    val s = n.toString
-    s == s.reverse
-  }
   def main(args:Array[String]) {
-    println{
-      {for(i <- 100 to 999;
-        j <- 100 to 999;
-        if(isPalindrome(i * j))) yield {i * j}}.max
-    }
+    print(998001 to 10000 by -1 find(n => n.toString==n.toString.reverse && (100 to 999 exists(x => n%x==0 && n/x<1000))))
   }
 }
