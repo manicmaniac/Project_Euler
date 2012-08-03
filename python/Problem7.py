1f0f57a40c49675bc42769fc04654e883a805e77
user <user@debian.(none)>
2012-08-03 19:07:48 +0900

    update

--- python/Problem7.py
+++ python/Problem7.py
@@ -4,8 +4,7 @@
 #By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #What is the 10001st prime number?
 
-if __name__ == '__main__':
-    import prime
-    for i, j in enumerate(prime.genprime()):
-        if i > 10000: break
-        print j
+from prime import genprime
+from itertools import islice
+
+print islice(genprime(), 10000, 10001).next()
