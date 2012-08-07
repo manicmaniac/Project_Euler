47f279af1de32dba2d30557947a30ce1bd2450d1
Ryosuke Ito <rito.0305@gmail.com>
2012-08-07 21:50:35 +0900

    update

--- scala/Problem31.scala
+++ scala/Problem31.scala
@@ -1,13 +1,14 @@
 object Problem31 {
-  val english_coins = List(200, 100, 50, 20, 10, 5, 2, 1)
-  def calculateCurrency(amount:Int, coins:List[Int]):Int =
+  val coins = List(200, 100, 50, 20, 10, 5, 2, 1)
+
+  def currency(amount:Int, coins:List[Int]):Int =
     (amount, coins) match {
       case (0, _) => 1
       case (_, Nil) => 0
       case (_, _) if(amount<0) => 0
-      case (_, _) => calculateCurrency(amount, coins.tail) + calculateCurrency(amount-coins.head, coins)
+      case (_, _) => currency(amount, coins.tail) + currency(amount-coins.head, coins)
   }
   def main(args:Array[String]) {
-    println(calculateCurrency(200, english_coins))
+    print(currency(200, coins))
   }
 }
