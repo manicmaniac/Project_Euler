8561f91d1a6fdb69767b4a70b1525d10b6bce3a8
Ryosuke Ito <rito.0305@gmail.com>
2012-08-09 11:18:26 +0900

    update

--- scala/Problem10.scala
+++ scala/Problem10.scala
@@ -4,29 +4,10 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 */
 
-/*
 object Problem10 {
-  def count(n:Int):Stream[Int] = {
-    Stream.cons(n, count(n + 1))
-  }
-  def genPrimes(xs:Stream[Int]):Stream[Int] = {
-    Stream.cons(xs.head, genPrimes(xs.tail.filter(_ % xs.head != 0)))
-  }
+  def from(n:Long):Stream[Long] = n #:: from(n+1)
+  val primeCandidates = 2L #:: 3L #:: from(1).map(_*6-1).zip(from(1).map(_*6+1)).map(p => List(p._1, p._2)).flatten
   def main(args:Array[String]) {
-    lazy val primes:Stream[Int] = genPrimes(count(2))
-    println(primes.takeWhile(_ < 10000).sum)
-  }
-}
-*/
-
-object Problem10 {
-  lazy val prime_candidates:Stream[Long] = Stream(2L, 3L) #::: primeCandidates(1)
-
-  def primeCandidates(n:Long):Stream[Long] = {
-    Stream.cons(6*n-1, Stream.cons(6*n+1, primeCandidates(n+1)))
-  }
-
-  def main(args:Array[String]) {
-    prime_candidates take 100 print
+    print(primeCandidates.filter(BigInt(_).isProbablePrime(10)).takeWhile(2000000>).sum)
   }
 }
