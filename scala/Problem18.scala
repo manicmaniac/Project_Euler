0c5c14c2324732127de46d606d80c382fac10f7d
Ryosuke Ito <rito.0305@gmail.com>
2012-08-09 21:13:55 +0900

    update

--- scala/Problem18.scala
+++ scala/Problem18.scala
@@ -11,22 +11,6 @@ That is, 3 + 7 + 4 + 9 = 23.
 
 Find the maximum total from top to bottom of the triangle below:
 
-75
-95 64
-17 47 82
-18 35 87 10
-20 04 82 47 65
-19 01 23 75 03 34
-88 02 77 73 07 63 67
-99 65 04 28 06 16 70 92
-41 41 26 56 83 40 80 70 33
-41 48 72 33 47 32 37 16 94 29
-53 71 44 65 25 43 91 52 97 51 14
-70 11 33 28 77 73 17 78 39 68 17 57
-91 71 52 38 17 14 91 43 58 50 27 29 48
-63 66 04 68 89 53 67 30 73 16 69 87 40 31
-04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
-
 NOTE: As there are only 16384 routes, it is possible to solve this problem by
 trying every route. However, Problem 67, is the same challenge with a triangle
 containing one-hundred rows; it cannot be solved by brute force, and requires a
@@ -55,6 +39,6 @@ object Problem18 {
     case _ => findRoute(pyramid.tail.head.zip(pyramid.head.zip(pyramid.head.tail)).map(p => p._1 + (if(p._2._1>p._2._2) p._2._1; else p._2._2))::pyramid.tail.tail)
   }
   def main(args:Array[String]) {
-    println(findRoute(pyramid.reverse))
+    print(findRoute(pyramid.reverse))
   }
 }
