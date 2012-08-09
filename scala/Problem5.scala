8f11b58e91fb4f1a3d197ceb59a3f8a31013a6de
Ryosuke Ito <rito.0305@gmail.com>
2012-08-09 19:52:34 +0900

    update

--- scala/Problem5.scala
+++ scala/Problem5.scala
@@ -2,13 +2,7 @@
 //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 object Problem5 {
-  def gcd(m:Long, n:Long):Long = n match {
-    case 0 => m
-    case _ if(m < n) => gcd(n, m)
-    case _ => gcd(n, m % n)
-  }
-  def lcm(m:Long, n:Long) = m * n / gcd(m, n)
   def main(args:Array[String]) {
-    print(1L to 20 reduce lcm)
+    print(BigInt(1) to 20 reduce((m, n) => m*n/m.gcd(n)))
   }
 }
