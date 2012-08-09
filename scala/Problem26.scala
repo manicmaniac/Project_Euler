2d8c22802c3eadef3cf25115b46e4ab8fda06f49
Ryosuke Ito <rito.0305@gmail.com>
2012-08-09 17:14:40 +0900

    update

---
+++ scala/Problem26.scala
@@ -0,0 +1,31 @@
+/*
+A unit fraction contains 1 in the numerator. The decimal representation of the
+unit fractions with denominators 2 to 10 are given:
+
+    ^1/[2]  =  0.5
+    ^1/[3]  =  0.(3)
+    ^1/[4]  =  0.25
+    ^1/[5]  =  0.2
+    ^1/[6]  =  0.1(6)
+    ^1/[7]  =  0.(142857)
+    ^1/[8]  =  0.125
+    ^1/[9]  =  0.(1)
+    ^1/[10] =  0.1
+
+Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle. It can be
+seen that ^1/[7] has a 6-digit recurring cycle.
+
+Find the value of d < 1000 for which ^1/[d] contains the longest recurring
+cycle in its decimal fraction part.
+*/
+
+object Problem26 {
+  def felm(n:Int) = {
+      for(d <- 1 to n) yield {
+        (1 until d find(BigInt(10).pow(_) % d == 1), d)
+      }
+    }.filter(_._1 != None).map(p => (p._1.get, p._2))
+  def main(args:Array[String]) {
+    print(felm(1001).max._2)
+  }
+}
