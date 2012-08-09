6dd2431970f7d33b2ef873b15dacaaa6ea014a04
Ryosuke Ito <rito.0305@gmail.com>
2012-08-09 21:13:01 +0900

    update

--- scala/Problem16.scala
+++ scala/Problem16.scala
@@ -2,18 +2,8 @@
 2 15  = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 What is the sum of the digits of the number 2 1000 ?
 */
-
 object Problem16 {
-  def pow(base:BigInt, n:Int, res:BigInt=1):BigInt = n match {
-    case 0 => res
-    case _ if(n<0) => 0
-    case _ => pow(base, n-1, res*base)
-  }
-  def sumDigits(n:BigInt) = {
-    n.toString.map(_.asDigit).sum
-  }
   def main(args:Array[String]) {
-    print(sumDigits(pow(2,1000)))
+    print(BigInt(2).pow(1000).toString.map(_.asDigit) sum)
   }
 }
-
