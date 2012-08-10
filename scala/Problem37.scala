63d8c4000e291477e97152274581247b7c98ac01
Ryosuke Ito <rito.0305@gmail.com>
2012-08-10 16:20:48 +0900

    update

---
+++ scala/Problem37.scala
@@ -0,0 +1,32 @@
+/*
+The number 3797 has an interesting property. Being prime itself, it is possible
+to continuously remove digits from left to right, and remain prime at each
+stage: 3797, 797, 97, and 7. Similarly we can work from right to left: 3797,
+379, 37, and 3.
+
+Find the sum of the only eleven primes that are both truncatable from left to
+right and right to left.
+
+NOTE: 2, 3, 5, and 7 are not considered to be truncatable primes.
+*/
+
+object Problem37 {
+  def trunc(n:Int) = {
+    (n.toString.tails.toList.init ++ n.toString.inits.toList.tail)
+    .filter(""!=).map(_.toInt)
+  }
+
+  def isProbablePrime(n:Int) = BigInt(n).isProbablePrime(10)
+
+  lazy val candidates = (9 to 1000000 by 2).toList
+    .filter(x => (x.toString.head.asDigit, x.toString.last.asDigit) match {
+      case _ if(x<100) => true
+      case (0|2|4|6|8, _) => false
+      case (_, 0|2|4|6|8) => false
+      case _ => true
+  })
+
+    def main(args:Array[String]) {
+      print(candidates.filter(trunc(_).forall(isProbablePrime)).take(11).sum)
+    }
+}
