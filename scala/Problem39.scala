0dbdebf223366613d31a6464613d39fb6b63fdf3
Ryosuke Ito <rito.0305@gmail.com>
2012-08-11 08:45:50 +0900

    update

---
+++ scala/Problem39.scala
@@ -0,0 +1,17 @@
+/*
+If p is the perimeter of a right angle triangle with integral length sides, {a,
+b,c}, there are exactly three solutions for p = 120.
+
+{20,48,52}, {24,45,51}, {30,40,50}
+
+For which value of p ≤ 1000, is the number of solutions maximised?
+*/
+
+object Problem39 {
+  def findPytagorean(perimeter:Int) = {
+    (1 to perimeter-1).flatMap(a => (1 to a).map(b => List(b, a, math.sqrt(a*a+b*b))).filter(_.sum == perimeter))
+  }
+  def main(args:Array[String]) {
+    print((1 to 1000).map(n => (findPytagorean(n).length, n)).max._2)
+  }
+}
