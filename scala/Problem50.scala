7ec038f10570e1ce81b0a690d71bac6ea7f712a6
Ryosuke Ito <rito.0305@gmail.com>
2012-08-11 19:19:45 +0900

    update

---
+++ scala/Problem50.scala
@@ -0,0 +1,24 @@
+/*
+The prime 41, can be written as the sum of six consecutive primes:
+
+41 = 2 + 3 + 5 + 7 + 11 + 13
+
+This is the longest sum of consecutive primes that adds to a prime below
+one-hundred.
+
+The longest sum of consecutive primes below one-thousand that adds to a prime,
+contains 21 terms, and is equal to 953.
+
+Which prime, below one-million, can be written as the sum of the most
+consecutive primes?
+*/
+
+object Problem50 {
+  val primes = 2 #:: Stream.from(1).map(_ * 2 + 1).filter(BigInt(_).isProbablePrime(10))
+
+  def isPrime(n:Int) = BigInt(n).isProbablePrime(10)
+
+  def main(args:Array[String]) {
+    print((546 to 1 by -1).flatMap(primes.take(546).sliding(_).map(_.sum).find(x => x<1000000 && isPrime(x))) max)
+  }
+}
