884ab91186cd44a564816a62df0e77fd95534364
Ryosuke Ito <rito.0305@gmail.com>
2012-08-11 12:35:09 +0900

    update

--- scala/Problem48.scala
+++ scala/Problem48.scala
@@ -5,6 +5,6 @@ Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 */
 object Problem48 {
   def main(args:Array[String]) {
-    print(((BigInt(1) to 1000).map(p => p.pow(p.toInt)).sum).toString.takeRight(10))
+    print((BigInt(1) to 1000 map(p => p pow p.toInt) sum).toString takeRight 10)
   }
 }
