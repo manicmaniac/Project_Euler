275a2d9f94f9b520ceae0dfbbb69ce796393d428
Ryosuke Ito <rito.0305@gmail.com>
2012-08-12 21:32:54 +0900

    update

--- python/Problem43.py
+++ python/Problem43.py
@@ -15,19 +15,21 @@ d8d9d10=289 is divisible by 17
 Find the sum of all 0 to 9 pandigital numbers with this property.
 '''
 
-from pandigital import gen_pandigital
-from prime import genprime
+candidates =  [int(''.join(map(str, [d1, d2, d3, d4, d5, d6, d7, d8, d9, d10])))
+        for d1 in range(1, 10)
+        for d2 in range(10)
+        for d3 in range(10)
+        for d4 in range(10) if not (d2 * 100 + d3 * 10 + d4) % 2
+        for d5 in range(10) if not (d3 * 100 + d4 * 10 + d5) % 3
+        for d6 in range(10) if not (d4 * 100 + d5 * 10 + d6) % 5
+        for d7 in range(10) if not (d5 * 100 + d6 * 10 + d7) % 7
+        for d8 in range(10) if not (d6 * 100 + d7 * 10 + d8) % 11
+        for d9 in range(10) if not (d7 * 100 + d8 * 10 + d9) % 13
+        for d10 in range(10) if not (d8 * 100 + d9 * 10 + d10) % 17]
 
-def check(n):
-    acc = 0
-    for i in range(1, 8):
-        acc += int(str(n)[i: i + 3]) % [j for j in genprime(20)][i - 1]
-    return not acc
+def is_pandigital(n):
+    return sorted(str(n)) == map(str, range(10))
+
 
 if __name__ == '__main__':
-    acc = 0
-    for i in gen_pandigital(0, 9):
-        if not int(str(i)[-3:]) % 17:
-            if check(i):
-                acc += i
-    print acc
+    print sum(filter(is_pandigital, candidates))
