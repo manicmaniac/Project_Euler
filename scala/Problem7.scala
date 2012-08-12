0be1671cb2b53604bd44ab8a43718b2b4e576433
Ryosuke Ito <rito.0305@gmail.com>
2012-08-12 15:50:16 +0900

    update

--- scala/Problem7.scala
+++ scala/Problem7.scala
@@ -4,12 +4,11 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 */
 
-import scala.math.sqrt
-
 object Problem7 {
-  lazy val primes = {
-    2 #:: Stream.from(2).filter(n => !((2::(3 to sqrt(n).toInt by 2).toList).exists(n%_==0)))
-  }
+  lazy val primes = 2 #:: Stream.from(1).map(_*2+1).filter(isPrime)
+
+  def isPrime(n:Int) = BigInt(n).isProbablePrime(10)
+
   def main(args:Array[String]) {
     print(primes(10000))
   }
