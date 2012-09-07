fb85588d5fc98499a8b9a6ee541dfd3285b4d090
Ryosuke Ito <rito.0305@gmail.com>
2012-09-08 00:41:48 +0900

    update

--- python/Problem48.py
+++ python/Problem48.py
@@ -1,7 +1,10 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
-#The series, 11 + 22 + 33 + ... + 1010 = 10405071317.
-#Find the last ten digits of the series, 11 + 22 + 33 + ... + 10001000.
+'''
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+'''
 
 print str(sum(i**i for i in range(1, 1001)))[-10:]
 
