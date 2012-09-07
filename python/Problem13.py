8d7df0572e5212d7e85eaafa324c23971c2f71b7
Ryosuke Ito <rito.0305@gmail.com>
2012-09-08 00:35:09 +0900

    update

--- python/Problem13.py
+++ python/Problem13.py
@@ -1,8 +1,9 @@
-#/usr/bin/env python
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 '''
 Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
 '''
+
 data = '''37107287533902102798797998220837590246510135740250
 46376937677490009712648124896970078050417018260538
 74324986199524741059474233309513058123726617309629
