0c4862fba23ee4674681f180ec6db6c74670c455
Ryosuke Ito <rito.0305@gmail.com>
2012-09-08 01:04:14 +0900

    update

--- python/Problem7.py
+++ python/Problem7.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+'''
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
+the 6th prime is 13.
 
-#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-#What is the 10001st prime number?
+What is the 10 001st prime number?
+'''
+                                                                               
+from sympy import *
+print sieve[10001]
 
-from sympy import sieve; print sieve[10001]
