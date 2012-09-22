d74b75dbf4f208e5ae9343f9220647129d7d18f5
Ryosuke Ito <rito.0305@gmail.com>
2012-09-23 01:24:31 +0900

    update

--- sh/Problem9.sh
+++ sh/Problem9.sh
@@ -8,15 +8,15 @@
 #There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 #Find the product abc.
 
-for m in `seq 23`
+for m in {1..23}
 do
-    for n in `seq 23` 
+    for n in {1..23}
     do
-        a=`expr $m \* $m - $n \* $n`
-        b=`expr 2 \* $m \* $n`
-        c=`expr $m \* $m + $n \* $n`
-        if [ `expr $a + $b + $c` -eq 1000 ]; then
-            echo `expr $a \* $b \* $c`
+        let a=$m*$m-$n*$n
+        let b=2*$m*$n
+        let c=$m*$m+$n*$n
+        if [ $(($a + $b + $c)) -eq 1000 ]; then
+            echo $(($a * $b * $c))
         fi
     done
 done
