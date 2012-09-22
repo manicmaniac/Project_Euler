59584617516c31eec4aaef572a0bcc7bb3ed8871
Ryosuke Ito <rito.0305@gmail.com>
2012-09-22 21:38:07 +0900

    update

---
+++ sh/Problem25.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#The Fibonacci sequence is defined by the recurrence relation:
+#
+#    F[n] = F[n−1] + F[n−2], where F[1] = 1 and F[2] = 1.
+#
+#Hence the first 12 terms will be:
+#
+#    F[1] = 1
+#    F[2] = 1
+#    F[3] = 2
+#    F[4] = 3
+#    F[5] = 5
+#    F[6] = 8
+#    F[7] = 13
+#    F[8] = 21
+#    F[9] = 34
+#    F[10] = 55
+#    F[11] = 89
+#    F[12] = 144
+#
+#The 12th term, F[12], is the first term to contain three digits.
+#
+#What is the first term in the Fibonacci sequence to contain 1000 digits?
+
+s=0
+t=1
+ans=1
+while [ `echo "$t < 10 ^ 999" | bc` -eq 1 ]
+do
+    tmp=$s
+    s=$t
+    t=`echo "$s + $tmp" | bc`
+    ans=`expr $ans + 1`
+done
+
+echo $ans
+
