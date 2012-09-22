8b63737005057f107d1da3e87deaac93443d3da8
Ryosuke Ito <rito.0305@gmail.com>
2012-09-22 11:24:48 +0900

    update

--- sh/Problem6.sh
+++ sh/Problem6.sh
@@ -13,40 +13,11 @@
 #Find the difference between the sum of the squares of the first one hundred
 #natural numbers and the square of the sum.
 
-function pow() {
-    local res=1
-    local i=1
-    while [ `expr $2 - $i` -ge 0 ]
-    do
-        res=`expr $res \* $1`
-        i=`expr $i + 1`
-    done
-    echo $res
-}
+for i in `seq 100`
+do
+    s=`expr $s + $i`
+    t=`expr $t + $i \* $i`
+done
+s=`expr $s \* $s`
+echo `expr $s - $t`
 
-function sum_square() {
-    local i=1
-    local res=0
-    while [ $i -le $1 ]
-    do
-        tmp=`pow $i 2`
-        res=`expr $res + $tmp`
-        i=`expr $i + 1`
-    done
-    echo $res
-}
-
-function square_sum() {
-    local i=1
-    local res=0
-    while [ $i -le $1 ]
-    do
-        res=`expr $res + $i`
-        i=`expr $i + 1`
-    done
-    echo `pow $res 2`
-}
-
-a=`square_sum 100`
-b=`sum_square 100`
-echo `expr $a - $b`
