a2923dbeeb4544a614b997d014bfbd7a99e8f00d
Ryosuke Ito <rito.0305@gmail.com>
2012-09-22 20:20:55 +0900

    update

---
+++ sh/Problem19.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#You are given the following information, but you may prefer to do some research
+#for yourself.
+#
+#  • 1 Jan 1900 was a Monday.
+#  • Thirty days has September,
+#    April, June and November.
+#    All the rest have thirty-one,
+#    Saving February alone,
+#    Which has twenty-eight, rain or shine.
+#    And on leap years, twenty-nine.
+#  • A leap year occurs on any year evenly divisible by 4, but not on a century
+#    unless it is divisible by 400.
+#
+#How many Sundays fell on the first of the month during the twentieth century (1
+#Jan 1901 to 31 Dec 2000)?
+
+function day() {
+    local y=$1
+    local m=$2
+    local d=$3
+    echo `env LANG=C date --date=$1$2$3 | cut -c -3`
+}
+
+ans=0
+for year in `seq 1901 2000`
+do
+    for month in `seq 12 | sed -e 's/\<.\>/0&/g'`
+    do
+        if [ `day $year $month 01` = 'Sun' ]; then
+            ans=`expr $ans + 1`
+        fi
+    done
+done
+echo $ans
+
