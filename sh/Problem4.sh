b44cae21e987df7304ac181b2cf233479b08c154
Ryosuke Ito <rito.0305@gmail.com>
2012-09-23 01:09:24 +0900

    unsolved

--- sh/Problem4.sh
+++ sh/Problem4.sh
@@ -4,27 +4,14 @@
 #
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
-function fib()
-{
-    local res
-    local s=0
-    local t=1
-    local tmp=0
-    while [ $t -lt $1 ]
+function is_palindromic() {
+    x=$1
+    xs=`echo $x | sed -e 's/./& /g'`
+    for i in $xs
     do
-        tmp=$s; s=$t; t=`expr $t + $tmp`
-        res+=($s)
+        echo $i
     done
-    echo ${res[@]}
 }
 
-ans=0
-for i in `fib 4000000`
-do
-    if [ `expr $i % 2` -eq 0 ]; then
-        ans=`expr $ans + $i`
-    fi
-done
-
-echo $ans
+is_palindromic 9001
 
