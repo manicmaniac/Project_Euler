b5aa17691422f03adcd83607e75f82716a7b0fb4
Ryosuke Ito <rito.0305@gmail.com>
2012-09-23 01:17:29 +0900

    update

--- sh/Problem5.sh
+++ sh/Problem5.sh
@@ -8,31 +8,27 @@
 function gcd() {
     local x=0
     local y=0
-    if [ `expr $1 - $2` -gt 0 ]; then
-        x=$1
-        y=$2
+    if [ $(($1 - $2)) -gt 0 ]; then
+        x=$1; y=$2
     else
-        x=$2
-        y=$1
+        x=$2; y=$1
     fi
-    while [ `expr $x % $y` -ne 0 ]
+    while [ $(($x % $y)) -ne 0 ]
     do
-        tmp=$x
-        x=$y
-        y=`expr $tmp % $y`
+        tmp=$x; x=$y
+        let y=$tmp%$y
     done
     echo $y
 }
 
 function lcm() {
-    echo `expr $(expr $1 \* $2) / $(gcd $1 $2)`
+    echo $(($(($1 * $2)) / $(gcd $1 $2)))
 }
 
-i=2
-res=2
+i=2; res=2
 while [ $i -lt 21 ]
 do
-    i=`expr $i + 1`
+    let i=$i+1
     res=`lcm $i $res`
 done
 echo $res
