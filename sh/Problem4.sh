690faf52d14011db93152b84c716ea14881a28a4
Ryosuke Ito <rito.0305@gmail.com>
2012-09-22 11:18:36 +0900

    update

--- sh/Problem4.sh
+++ sh/Problem4.sh
@@ -4,3 +4,27 @@
 #
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
+function fib()
+{
+    local res
+    local s=0
+    local t=1
+    local tmp=0
+    while [ $t -lt $1 ]
+    do
+        tmp=$s; s=$t; t=`expr $t + $tmp`
+        res+=($s)
+    done
+    echo ${res[@]}
+}
+
+ans=0
+for i in `fib 4000000`
+do
+    if [ `expr $i % 2` -eq 0 ]; then
+        ans=`expr $ans + $i`
+    fi
+done
+
+echo $ans
+
