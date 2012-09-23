6d907034235969d6ac1c23d3adde9c87ccf9e50b
Ryosuke Ito <rito.0305@gmail.com>
2012-09-23 12:59:10 +0900

    update

--- sh/Problem7.sh
+++ sh/Problem7.sh
@@ -4,42 +4,38 @@
 #
 #What is the 10 001st prime number?
 
-function is_prime {
-    if [ $1 -lt 2 ]; then
+function is_prime() {
+    local x=$1
+    if [ $x -lt 2 ]; then
+        return 1
+    else
+        for i in `seq 2 $(echo "sqrt($x) + 1" | bc)`
+        do
+            if [ $(($x % $i)) -eq 0 ]; then
+                return 1
+            fi
+        done
         return 0
     fi
-    local i=2
-    while [ `expr $1 - $i \* $i` -ge 0 ]
-    do
-        if [ `expr $1 % $i` -eq 0 ]; then
-            return 1
-        fi
-        i=`expr $i + 1`
-    done
-    return 0
 }
 
-function find_prime {
-    local i=1
-    local count_prime=2
-    local primes=(2 3)
-    while [ `expr $1 - $count_prime` -ge 0 ]
-    do
-        s=`expr $i \* 6 - 1`
-        is_prime $s && primes+=($s) && count_prime=`expr $count_prime + 1`
-        if [ `expr $1 - $count_prime` -le 0 ]; then
-            echo $primes
-            return 0
+i=1; cnt=2; limit=10001
+while true 
+do
+    if `is_prime $(($i * 6 - 1))`; then
+        let cnt++
+        if [ $cnt -ge $limit ]; then
+            echo $(($i * 6 - 1))
+            break
         fi
-        t=`expr $i \* 6 + 1`
-        is_prime $t && primes+=($t) && count_prime=`expr $count_prime + 1`
-        i=`expr $i + 1`
-    done
-    for var in $primes
-    do
-        print $var
-    done
-}
+    fi
+    if `is_prime $(($i * 6 + 1))`; then
+        let cnt++
+        if [ $cnt -ge $limit ]; then
+            echo $(($i * 6 + 1))
+            break
+        fi
+    fi
+    let i++
+done
 
-is_prime 6 && echo "true"
-find_prime 10
