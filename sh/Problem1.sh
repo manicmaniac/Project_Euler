80dd82fe788254f166cd722b0e35f9a81e10de07
Ryosuke Ito <rito.0305@gmail.com>
2012-10-06 17:35:39 +0900

    update

--- sh/Problem1.sh
+++ sh/Problem1.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#If we list all the natural numbers below 10 that are multiples of 3 or 5, we
-#get 3, 5, 6 and 9. The sum of these multiples is 23.
-#
-#Find the sum of all the multiples of 3 or 5 below 1000.
+:<<'###'
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+###
 
 tr ' ' + <<<`sort -mnu <(seq 3 3 999) <(seq 5 5 999)` | bc
 
