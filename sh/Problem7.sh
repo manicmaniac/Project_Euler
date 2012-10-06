0c0f8b29a89ed4ce5f7639320f0245b7fa8ba2f0
Ryosuke Ito <rito.0305@gmail.com>
2012-10-06 19:04:04 +0900

    update

--- sh/Problem7.sh
+++ sh/Problem7.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
-#the 6th prime is 13.
-#
-#What is the 10 001st prime number?
+:<<'###'
+By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
+the 6th prime is 13.
+
+What is the 10 001st prime number?
+###
 
 function is_prime() {
     local x=$1
