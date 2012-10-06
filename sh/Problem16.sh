5de8ef4c969961f09d236da4d228028991354f5a
Ryosuke Ito <rito.0305@gmail.com>
2012-10-06 19:25:51 +0900

    update

--- sh/Problem16.sh
+++ sh/Problem16.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-#2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-#
-#What is the sum of the digits of the number 2^1000?
+:<<'###'
+2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 
-echo '2^1000' | bc | paste -s | sed -e 's/\\\s*//g;s/./&+/g;s/+$//' | bc
+What is the sum of the digits of the number 2^1000?
+###
+
+echo `bc <<<2^1000` | sed -e 's/\\\s//g;s/./&+/g;s/+$//' | bc
 
