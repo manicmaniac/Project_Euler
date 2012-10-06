3376fcb787147c3a4a4b7c1d6f04cbe14a4b7646
Ryosuke Ito <rito.0305@gmail.com>
2012-10-06 19:19:52 +0900

    update

--- sh/Problem13.sh
+++ sh/Problem13.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#Work out the first ten digits of the sum of the following one-hundred 50-digit
-#numbers.
+:<<'###'
+Work out the first ten digits of the sum of the following one-hundred 50-digit
+numbers.
+###
 
 sed -e '1,99s/$/+/g' <<__EOD__ | paste -s | bc | colrm 11
 37107287533902102798797998220837590246510135740250
