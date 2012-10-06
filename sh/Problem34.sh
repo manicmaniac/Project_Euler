1ac17dcc629704c47e4ab5562e459d510177c35a
Ryosuke Ito <rito.0305@gmail.com>
2012-10-06 20:33:04 +0900

    update

--- sh/Problem34.sh
+++ sh/Problem34.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
-#
-#Find the sum of all numbers which are equal to the sum of the factorial of
-#their digits.
-#
-#Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+:<<'###'
+145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+Find the sum of all numbers which are equal to the sum of the factorial of
+their digits.
+
+Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+###
 
 factorial=(0 1 2 6 24 120 720 5040 40320 362880)
 
