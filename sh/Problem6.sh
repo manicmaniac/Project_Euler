11afef31c0a2759ed93af9f03443b3f175e0b4c2
Ryosuke Ito <rito.0305@gmail.com>
2012-10-06 12:50:42 +0900

    update

--- sh/Problem6.sh
+++ sh/Problem6.sh
@@ -13,5 +13,5 @@
 #Find the difference between the sum of the squares of the first one hundred
 #natural numbers and the square of the sum.
 
-echo "`echo {1..100} | sed -e 's/\s/+/g;s/^/(/;s/$/)^2/'` - `echo {1..100} | sed -e 's/\s/^2-/g;s/$/^2/'`" | bc
+echo $(echo "(`seq -s + 1 100`)^2" | bc) - $(echo "`seq -s ^2+ 1 100`^2" | bc) | bc
 
