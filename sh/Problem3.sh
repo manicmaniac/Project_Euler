693d9b4737b5775b1a21d93eb63a5777450ae836
Ryosuke Ito <rito.0305@gmail.com>
2012-10-06 18:37:35 +0900

    update

--- sh/Problem3.sh
+++ sh/Problem3.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
-#The prime factors of 13195 are 5, 7, 13 and 29.
-#
-#What is the largest prime factor of the number 600851475143 ?
+:<<'###'
+The prime factors of 13195 are 5, 7, 13 and 29.
 
-function factorize() {
-    local {n=$1,i=2}
-    while [ $(($n - $i * $i)) -gt 0 ]
-    do
-        if [ $(($n % $i)) -eq 0 ]; then
-            let n/=$i
-            echo -n "$i "
-        else
-            let i++
-        fi
-    done
-    echo -n $n
-}
+What is the largest prime factor of the number 600851475143 ?
+###
 
-ans=(`factorize 600851475143`)
-let last=${#ans[*]}-1
-echo ${ans[$last]}
+factors=(`factor 600851475143`)
+
+echo ${factors[-1]}
 
