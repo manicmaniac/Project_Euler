b5b2d62236632ba18a8c4741025bbc5d73b0d136
Ryosuke Ito <rito.0305@gmail.com>
2012-10-06 19:23:11 +0900

    update

--- sh/Problem15.sh
+++ sh/Problem15.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-#Starting in the top left corner of a 2×2 grid, there are 6 routes (without
-#backtracking) to the bottom right corner.
-#
-#[p_015]
-#
-#How many routes are there through a 20×20 grid?
+:<<'###'
+Starting in the top left corner of a 2×2 grid, there are 6 routes (without
+backtracking) to the bottom right corner.
+
+[p_015]
+
+How many routes are there through a 20×20 grid?
+###
 
 function c() {
     local {a=$1,b=$2,multi=1,fact=1}
     local start=$(($b + 1))
     for i in `seq $start $a`
     do
-        multi=`echo "$multi * $i" | bc`
+        multi=`bc <<<"$multi * $i"`
     done
     for j in `seq $b`
     do
-        fact=`echo "$fact * $j" | bc`
+        fact=`bc <<<"$fact * $j"`
     done
-    echo `echo "$multi / $fact" | bc`
+    echo `bc <<<"$multi / $fact"`
 }
 
 c 40 20
