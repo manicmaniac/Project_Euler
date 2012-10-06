d6817f90c6a1daac0a94c413e7485203d10f0d3c
Ryosuke Ito <rito.0305@gmail.com>
2012-10-06 20:36:36 +0900

    update

--- sh/Problem48.sh
+++ sh/Problem48.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-#The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
-#
-#Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+:<<'###'
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+###
 
 res=$(echo 0 `seq 1000 | sed -e 's/.*/+&^&/g'` | bc | tail -2 | paste -s | sed 's/\\\s*//g')
 
-last=`echo $res | wc -m`
-start=`expr $last - 10`
+last=`wc -m <<<$res`
+start=$(($last - 10))
 
 echo $res | cut -c $start-$last
 
