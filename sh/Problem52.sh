0c709b0394f91985749a21a7ae879098440a8a16
Ryosuke Ito <rito.0305@gmail.com>
2012-10-06 20:37:09 +0900

    update

--- sh/Problem52.sh
+++ sh/Problem52.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#It can be seen that the number, 125874, and its double, 251748, contain exactly
-#the same digits, but in a different order.
-#
-#Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x,
-#contain the same digits.
+:<<'###'
+It can be seen that the number, 125874, and its double, 251748, contain exactly
+the same digits, but in a different order.
+
+Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x,
+contain the same digits.
+###
 
 function make_dialnumber() {
     local {x=$1,res}
