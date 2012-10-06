f94f2c0f7d84deeccd9a3b406425070d5aaf6205
Ryosuke Ito <rito.0305@gmail.com>
2012-10-06 19:02:49 +0900

    update

--- sh/Problem7.sh
+++ sh/Problem7.sh
@@ -6,36 +6,22 @@
 
 function is_prime() {
     local x=$1
-    if [ $x -lt 2 ]; then
-        return 1
-    else
-        for i in `seq 2 $(echo "sqrt($x) + 1" | bc)`
-        do
-            if [ $(($x % $i)) -eq 0 ]; then
-                return 1
-            fi
-        done
-        return 0
-    fi
+    local -a factors="(`factor $x`)"
+    test ${#factors[*]} -eq 2
 }
 
-i=1; cnt=2; limit=10001
-while : 
-do
-    if `is_prime $(($i * 6 - 1))`; then
-        let cnt++
-        if [ $cnt -ge $limit ]; then
-            echo $(($i * 6 - 1))
-            break
+function nth_prime() {
+    local {n=$1,i=3}
+    echo 2
+    while [ $n -gt 1 ]
+    do
+        if is_prime $i; then
+            echo $i
+            let n--
         fi
-    fi
-    if `is_prime $(($i * 6 + 1))`; then
-        let cnt++
-        if [ $cnt -ge $limit ]; then
-            echo $(($i * 6 + 1))
-            break
-        fi
-    fi
-    let i++
-done
+        let i+=2
+    done
+}
+
+nth_prime 10001 | tail -1
 
