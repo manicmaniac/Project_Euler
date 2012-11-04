62a84b7f79b997e78ad98f44543123f1c745ff4d
Ryosuke Ito <rito.0305@gmail.com>
2012-11-05 07:39:39 +0900

    update

--- python/Problem19.py
+++ python/Problem19.py
@@ -1,26 +1,24 @@
+#!/usr/bin/env python
 # -*- coding:utf-8 -*-
 '''
-You are given the following information, but you may prefer to do some research for yourself.
+You are given the following information, but you may prefer to do some research
+for yourself.
 
-    1 Jan 1900 was a Monday.
-    Thirty days has September,
+  • 1 Jan 1900 was a Monday.
+  • Thirty days has September,
     April, June and November.
     All the rest have thirty-one,
     Saving February alone,
     Which has twenty-eight, rain or shine.
     And on leap years, twenty-nine.
-    A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+  • A leap year occurs on any year evenly divisible by 4, but not on a century
+    unless it is divisible by 400.
 
-How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+How many Sundays fell on the first of the month during the twentieth century (1
+Jan 1901 to 31 Dec 2000)?
 '''
 
-import calendar
+from datetime import date
 
-class Problem19(calendar.Calendar):
-    def isFirstDayMonday(self, year, month):
-        return self.itermonthdays2(year, month).next() == (1, 6)
-
-if __name__ == '__main__':
-    p = Problem19(6)
-    print sum([p.isFirstDayMonday(i, j) for i in range(1901, 2001) for j in range(1, 13)])
+print sum(date(y, m, 1).weekday() == 6 for y in range(1901, 2001) for m in range(1, 13))
 
