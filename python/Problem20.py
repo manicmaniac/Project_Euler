aa4911697bcec7db3686478640752dff953a2b95
Ryosuke Ito <rito.0305@gmail.com>
2012-11-12 03:50:57 +0900

    update

--- python/Problem20.py
+++ python/Problem20.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 '''
-n! means n $B!_(B (n $B!](B 1) $B!_(B ... $B!_(B 3 $B!_(B 2 $B!_(B 1
+n! means n × (n − 1) × ... × 3 × 2 × 1
 
-For example, 10! = 10 $B!_(B 9 $B!_(B ... $B!_(B 3 $B!_(B 2 $B!_(B 1 = 3628800,
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
 and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
 Find the sum of the digits in the number 100!
