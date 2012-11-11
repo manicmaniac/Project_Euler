813dfb11785c16f3432d56a622c1ae27adc98a93
Ryosuke Ito <rito.0305@gmail.com>
2012-11-11 15:13:01 +0900

    update

---
+++ c++/Problem3.cpp
@@ -0,0 +1,29 @@
+/*
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+*/
+
+#include <iostream>
+using namespace std;
+
+int maxFactor(long n) {
+	int d = 2;
+	int res = 0;
+	while (n >= d) {
+		if (n % d) {
+			d++;
+		}
+		else {
+			n /= d;
+			res = d;
+		}
+	}
+	return res;
+}
+
+int main() {
+	cout << maxFactor(600851475143L);
+	return 0;
+}
+
