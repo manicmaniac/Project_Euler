cdc5f9181e86f48fd4570a17b0641a3095a67bde
Ryosuke Ito <rito.0305@gmail.com>
2012-11-13 23:20:56 +0900

    update

--- python/Problem3.py
+++ python/Problem3.py
@@ -5,6 +5,6 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 '''
 
-from sympy import *
+from sympy import factorint
 print max(factorint(600851475143))
 
