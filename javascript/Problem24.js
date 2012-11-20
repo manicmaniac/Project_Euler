80f8eb069abc0e62c349e806bc37fb7db24ca593
Ryosuke Ito <rito.0305@gmail.com>
2012-11-21 00:10:22 +0900

    update

--- javascript/Problem24.js
+++ javascript/Problem24.js
@@ -10,38 +10,7 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5,
 6, 7, 8 and 9?
 */
 
-function divisors(n) {
-  var res = [];
-  for (var i=1; i<n; i++) {
-    res += xs[i];
-  }
-  return res;
-};
+var itertools = require('itertools');
 
-function isAbundant(n) {
-  return sum(divisors(n)) > n;
-};
-
-var abundants = [];
-for (var i=12; i<28111; i++) {
-  if (isAbundant(i)) {
-    abundants[abundants.length] = i;
-  };
-};
-
-function isMadeBy2Abundants(n) {
-  for (var i=0; abundants[i]; i++) {
-    if (n < abundants[i]) break;
-    if (isAbundant(n - abundants[i])) return true;
-  };
-  return false;
-};
-
-
-var ans = 0;
-for (var i=1; i<28123; i++) {
-  if(!isMadeBy2Abundants(i)) ans += i;
-};
-
-console.log(ans);
+console.log(itertools.permutationsSync([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])[1e6-1].join(''));
 
