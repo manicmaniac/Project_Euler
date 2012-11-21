87e17c7ac1e4819819827b4554310ca5b7030dda
Ryosuke Ito <rito.0305@gmail.com>
2012-11-21 22:06:24 +0900

    update

---
+++ perl/Problem3.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/env perl -l
+=pod
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+=cut
+
+use strict;
+use warnings;
+
+sub factor {
+    my ($n, $d, @res) = ($_[0], 2);
+    while ($n >= $d) {
+        unless ($n % $d) {
+            push @res, $d;
+            $n /= $d;
+        }
+        else {
+            $d ++;
+        }
+    }
+    return \@res;
+}
+
+print ${factor(600851475143)}[-1];
+
