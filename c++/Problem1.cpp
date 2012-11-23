78281839d2dc36df4d315e0c5883a0a5a8d67645
Ryosuke Ito <rito.0305@gmail.com>
2012-11-23 15:05:56 +0900

    update

--- c++/Problem1.cpp
+++ c++/Problem1.cpp
@@ -8,11 +8,12 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 #include <iostream>
 using namespace std;
 
-int main() {
-	int res = 0;
-	for(int i=1; i<1000; i++)
-		if(i%3==0||i%5==0)
-			res += i;
-	cout << res;
+int main(int argc, char const* argv[]) {
+	int ans = 0;
+	for(int i=0; i<1e3; i++) {
+		ans += i % 3 * i % 5 ? 0 : i;
+	}
+	cout << ans;
 	return 0;
 }
+
