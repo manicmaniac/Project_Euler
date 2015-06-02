9eca1876e111732254fd2c4d0c8b63301e8010ec
Ryosuke Ito <rito.0305@gmail.com>
2015-06-03 06:05:01 +0900

    update

--- python/Problem48.py
+++ python/Problem48.py
@@ -6,5 +6,5 @@ The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
 Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 '''
 
-print str(sum(i**i for i in range(1, 1001)))[-10:]
+print sum(pow(i, i, 10 ** 10) for i in range(1, 1001)) % 10 ** 10
 
