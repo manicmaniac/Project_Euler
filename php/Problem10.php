7d6bde18bd8d45cf41ae2d8f0f1af427398454d8
Ryosuke Ito <rito.0305@gmail.com>
2015-06-13 12:08:29 +0900

    update

---
+++ php/Problem10.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ */
+
+function sieve($limit) {
+    if ($limit < 2) {
+        return [];
+    }
+    $search = array_fill(2, $limit - 2, true);
+
+    $sqrt_limit = sqrt($limit);
+    for ($current = 2; $current <= $sqrt_limit; $current++) {
+        for ($i = $current * 2; $i < $limit; $i += $current) {
+            unset($search[$i]);
+        }
+    }
+    return array_keys($search);
+}
+
+print_r(array_sum(sieve(2000000)));
+
+?>
+
