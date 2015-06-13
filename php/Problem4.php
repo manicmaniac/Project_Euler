3e7e4d043c011468208dd63b954929d0a503a83d
Ryosuke Ito <rito.0305@gmail.com>
2015-06-13 11:10:02 +0900

    update

---
+++ php/Problem4.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91  99.
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+
+function is_palindromic($x) {
+    return $x == strrev($x);
+}
+
+function is_3digits_product($x) {
+    for ($i = 100; $i <= sqrt($x); $i++) {
+        $quot = $x / $i;
+        $rem = $x % $i;
+        if (!$rem && $quot < 1000) {
+            return true;
+        }
+    }
+    return false;
+}
+
+for ($i = 998001; $i >= 10000; $i--) {
+    if (is_palindromic($i) && is_3digits_product($i)) {
+        echo $i;
+        break;
+    }
+}
+
+?>
+
