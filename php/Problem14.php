238f1e60db497fe5842281a281e938cb035c405d
Ryosuke Ito <rito.0305@gmail.com>
2015-06-24 00:40:50 +0900

    update

---
+++ php/Problem14.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * The following iterative sequence is defined for the set of positive integers:
+ * 
+ * n → n/2 (n is even)
+ * n → 3n + 1 (n is odd)
+ * 
+ * Using the rule above and starting with 13, we generate the following sequence:
+ * 
+ * 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+ * 
+ * It can be seen that this sequence (starting at 13 and finishing at 1) contains
+ * 10 terms. Although it has not been proved yet (Collatz Problem), it is thought
+ * that all starting numbers finish at 1.
+ * 
+ * Which starting number, under one million, produces the longest chain?
+ * 
+ * NOTE: Once the chain starts the terms are allowed to go above one million.
+ */
+
+function max_cached_collatz($n) {
+    $cache = [];
+    $longest = [0, 0];
+    foreach (range(1, $n, 2) as $start) {
+        $length = 1;
+        $i = $start;
+        while ($i > 1) {
+            if ($i & 1) {
+                $i = 3 * $i + 1;
+            } else {
+                $i >>= 1;
+            }
+            if (array_key_exists($i, $cache)) {
+                $length += $cache[$i];
+                break;
+            }
+            $length += 1;
+        }
+        if ($longest[1] < $length) {
+            $longest = [$start, $length];
+        }
+        $cache[$start] = $length;
+    }
+    return $longest;
+}
+
+echo max_cached_collatz(1000000)[0];
+
+?>
+
