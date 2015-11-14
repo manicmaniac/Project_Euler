46ac9f444658defcf08c5392f2213e59215aa722
Ryosuke Ito <rito.0305@gmail.com>
2015-11-14 10:10:40 +0900

    update

--- c/Problem10.c
+++ c/Problem10.c
@@ -1,54 +1,55 @@
+/*
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <math.h>
 
+static const int limit = 2000000;
+
 int *sieve(int limit, size_t *size) {
-    enum {
-        UNKNOWN = 0,
-        NONPRIME
-    };
+    int *search, *result;
+    int i, j;
+    double sqrt_limit;
+
     if (limit < 2) {
         return NULL;
     }
-    char *search = (char *)calloc(limit, sizeof(char));
-    search[0] = NONPRIME;
-    search[1] = NONPRIME;
-    int current;
-    int i;
-    double sqrt_limit = sqrt(limit);
-    for (current = 2; current <= sqrt_limit; current++) {
-        for (i = current << 1; i < limit; i += current) {
-            search[i] = NONPRIME;
-        }
-    }
+    search = calloc(limit, sizeof(int));
+    result = malloc(limit * sizeof(int));
+    search[0] = 1;
+    search[1] = 1;
+    sqrt_limit = sqrt(limit);
     *size = 0;
-    int *sparse_res = (int *)malloc(limit * sizeof(int));
-    for (i = 0; i < limit; i++) {
-        if (search[i] == UNKNOWN) {
-            sparse_res[*size] = i;
+    for (i = 2; i <= limit; i++) {
+        if (!search[i]) {
+            if (i <= sqrt_limit) {
+                for (j = i << 1; j < limit; j += i) {
+                    search[j] = 1;
+                }
+            }
+            result[*size] = i;
             *size += 1;
         }
     }
     free(search);
-    int *res = (int *)malloc(*size * sizeof(int));
-    memcpy(res, sparse_res, *size * sizeof(int));
-    free(sparse_res);
-    return res;
+    return realloc(result, *size * sizeof(int));
 }
 
-int main(int argc, char const* argv[])
-{
-    const int limit = 2000000;
-    size_t size;
-    int *sieved = sieve(limit, &size);
-    int i;
-    long res = 0;
+int main(int argc, const char *argv[]) {
+    size_t i, size;
+    int *primes;
+    long res;
+
+    primes = sieve(limit, &size);
+    res = 0;
     for (i = 0; i < size; i++) {
-        res += sieved[i];
+        res += primes[i];
     }
-    free(sieved);
+    free(primes);
     printf("%ld\n", res);
     return 0;
 }
-
