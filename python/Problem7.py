1c2e6b63f9c25e6d9207aa4ef04480a05b483bf2
Ryosuke Ito <rito.0305@gmail.com>
2015-11-15 13:34:52 +0900

    update

--- python/Problem7.py
+++ python/Problem7.py
@@ -1,13 +1,37 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-'''
+r"""
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that
 the 6th prime is 13.
 
 What is the 10 001st prime number?
-'''
-                                                                               
-from sympy import sieve
+"""
 
-print sieve[10001]
+import math
+import itertools
+import sys
 
+
+def prime_numbers(x):
+    return x / math.log(x)
+
+
+def sieve(limit):
+    if sys.version_info < (3,):
+        range = xrange
+    search = bytearray([1] * limit)
+    search[0] = 0
+    search[1] = 0
+    sqrt_limit = int(math.sqrt(limit))
+    for i in range(2, sqrt_limit):
+        if search[i]:
+            for j in range(i << 1, limit, i):
+                search[j] = 0
+    return itertools.compress(range(limit), search)
+
+
+if __name__ == '__main__':
+    for i in itertools.count(2):
+        if prime_numbers(i) > 10001:
+            print(next(itertools.islice(sieve(i), 10000, 10001)))
+            break
