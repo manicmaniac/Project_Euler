2907b86197eef41450a7888ee65e9cd72b6631ad
Ryosuke Ito <rito.0305@gmail.com>
2015-12-14 13:27:48 +0900

    update

--- python/Problem28.py
+++ python/Problem28.py
@@ -14,31 +14,6 @@ It can be verified that the sum of the numbers on the diagonals is 101.
 What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
 '''
 
-from itertools import count, islice, chain
-
-# N   E
-#  \ /
-#   *
-#  / \
-# W   S
-
-def north():
-    for n in count():
-        yield (2 * n + 1) ** 2 - 2 * n
-
-def east():
-    for n in count():
-        yield (2 * n + 1) ** 2
-
-def south():
-    for n in count():
-        yield (2 * n + 1) ** 2 - 6 * n
-
-def west():
-    for n in count():
-        yield (2 * n + 1) ** 2 - 4 * n
-
-
 if __name__ == '__main__':
-    print sum(chain(*(islice(i(), 1, 501) for i in [north, east, south, west]))) + 1
-
+    x = 1001 / 2
+    print(2 * (x + 1) * (8 * x * x + 7 * x + 6) / 3 - 3)
