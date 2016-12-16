97217bb8329542de9977e08f1c04f4a69b7b5b35
Ryosuke Ito <rito.0305@gmail.com>
2016-12-16 18:06:50 +0900

    update

--- c/Problem38.c
+++ c/Problem38.c
@@ -1,13 +1,13 @@
 /*
  * Take the number 192 and multiply it by each of 1, 2, and 3:
- * 
+ *
  * 192  1 = 192
  * 192  2 = 384
  * 192  3 = 576
  * By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product of 192 and (1,2,3)
- * 
+ *
  * The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645, which is the concatenated product of 9 and (1,2,3,4,5).
- * 
+ *
  * What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with (1,2, ... , n) where n  1?
  */
 #include <stdio.h>
@@ -37,7 +37,7 @@ int concat(int x, int y) {
     return x * pow(10, r) + y;
 }
 
-int main(int argc, char const* argv[]) {
+int main(int argc, const char *argv[]) {
     const int limit = 9877;
     int max, i, j, acc;
     max = 0;
