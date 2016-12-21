fc0409c4a33053eac8ad4dfa273eef9d0550ff1f
Ryosuke Ito <rito.0305@gmail.com>
2016-12-22 00:18:15 +0900

    update

---
+++ rust/Problem28.rs
@@ -0,0 +1,17 @@
+/*
+ * Starting with the number 1 and moving to the right in a clockwise direction a 5 by 5 spiral is formed as follows:
+ *
+ * 21 22 23 24 25
+ * 20  7  8  9 10
+ * 19  6  1  2 11
+ * 18  5  4  3 12
+ * 17 16 15 14 13
+ *
+ * It can be verified that the sum of the numbers on the diagonals is 101.
+ *
+ * What is the sum of the numbers on the diagonals in a 1001 by 1001 spiral formed in the same way?
+ */
+fn main() {
+    let x = 1001 / 2;
+    println!("{}", 2 * (x + 1) * (8 * x * x + 7 * x + 6) / 3 - 3);
+}
