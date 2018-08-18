55b98ef7507f8822c1c3cfa67539cab8c73bf376
Ryosuke Ito <rito.0305@gmail.com>
2018-08-19 03:13:58 +0900

    update

--- java/Problem7.java
+++ java/Problem7.java
@@ -6,41 +6,14 @@
  */
 
 import java.math.BigInteger;
-import java.util.Iterator;
-import java.util.Spliterator;
-import java.util.Spliterators;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class Problem7 {
-    public static class Prime implements Iterable<BigInteger> {
-        @Override
-        public Iterator<BigInteger> iterator() {
-            return new PrimeIterator();
-        }
-
-        private static class PrimeIterator implements Iterator<BigInteger> {
-            private BigInteger current = BigInteger.ONE;
-
-            @Override
-            public boolean hasNext() {
-                return true;
-            }
-
-            @Override
-            public BigInteger next() {
-                current = current.nextProbablePrime();
-                return current;
-            }
-        }
-    }
-
     public static void main(String args[]) {
-        Prime prime = new Prime();
-        Spliterator<BigInteger> spliterator = Spliterators.spliteratorUnknownSize(prime.iterator(),
-                    Spliterator.DISTINCT | Spliterator.IMMUTABLE | Spliterator.NONNULL | Spliterator.ORDERED | Spliterator.SORTED);
-        Stream<BigInteger> stream = StreamSupport.stream(spliterator, false);
-        BigInteger answer = stream.skip(10000).findFirst().get();
+        BigInteger answer = Stream.iterate(BigInteger.valueOf(2), BigInteger::nextProbablePrime)
+                .skip(10000)
+                .findFirst()
+                .get();
         System.out.println(answer);
     }
 }
