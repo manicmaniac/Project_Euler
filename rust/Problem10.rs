3fc5e96c2bbe06383e12c4e907bd7c5bdaad8660
Ryosuke Ito <rito.0305@gmail.com>
2018-11-28 01:50:12 +0900

    update

--- rust/Problem10.rs
+++ rust/Problem10.rs
@@ -4,31 +4,25 @@
  * Find the sum of all the primes below two million.
  */
 
-fn sieve(limit: usize) -> Vec<i64> {
-    let mut is_primes: Vec<bool> = Vec::with_capacity(limit);
-    for _ in 0..limit {
-        is_primes.push(true);
-    }
-    is_primes[0] = false;
-    is_primes[1] = false;
-    let sqrt_limit = (limit as f64).sqrt() as usize;
+fn sieve(is_composites: &mut [bool]) {
+    is_composites[0] = true;
+    is_composites[1] = true;
+    let sqrt_limit = (is_composites.len() as f64).sqrt() as usize;
     for i in 2..sqrt_limit {
-        if is_primes[i] {
-            for j in i..(((limit as f64) / i as f64).ceil() as usize) {
-                is_primes[j * i] = false
+        if !is_composites[i] {
+            for j in i..(((is_composites.len() as f64) / i as f64).ceil() as usize) {
+                is_composites[j * i] = true;
             }
         }
     }
-    let mut result: Vec<i64> = Vec::new();
-    for (i, &is_prime) in is_primes.iter().enumerate() {
-        if is_prime {
-            result.push(i as i64);
-        }
-    }
-    result
 }
 
 fn main() {
-    let primes = sieve(2000000);
-    println!("{}", primes.iter().fold(0, |x, y| x + y));
+    let mut is_composites = [false; 2000000];
+    sieve(&mut is_composites);
+    let answer: usize = is_composites.iter()
+        .enumerate()
+        .filter_map(|(i, &is_composite)| if is_composite { None } else { Some(i) })
+        .sum();
+    println!("{}", answer);
 }
