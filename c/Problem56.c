36ba74966fbf95cbdf36d9efa28a16b005614083
Ryosuke Ito <rito.0305@gmail.com>
2018-12-02 03:28:19 +0900

    update

--- c/Problem56.c
+++ c/Problem56.c
@@ -6,39 +6,144 @@
  * Considering natural numbers of the form, a^b, where a, b < 100, what is the
  * maximum digital sum?
  */
+#include <math.h>
 #include <stdio.h>
-#include <gmp.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define DIGITS_COUNT_OF_99_99 198 /* log10(99) * 99 + 1 */
+typedef struct {
+    int *digits;
+    size_t digits_count;
+} bignum;
+
+bignum *bignum_new(int x) {
+    bignum *a;
+    size_t digits_count, i;
+    div_t dm;
+
+    digits_count = (size_t)log10(x) + 1;
+    a = malloc(sizeof(bignum));
+    a->digits_count = digits_count;
+    a->digits = calloc(digits_count, sizeof(int));
+    dm.quot = x;
+    for (i = 0; i < digits_count; i++) {
+        dm = div(dm.quot, 10);
+        a->digits[i] = dm.rem;
+    }
+    return a;
+}
+
+bignum *bignum_copy(bignum *a) {
+    bignum *b;
+
+    b = malloc(sizeof(bignum));
+    b->digits_count = a->digits_count;
+    b->digits = malloc(b->digits_count * sizeof(int));
+    memcpy(b->digits, a->digits, b->digits_count * sizeof(int));
+    return b;
+}
+
+void bignum_delete(bignum *a) {
+    free(a->digits);
+    free(a);
+}
+
+void bignum_carry(bignum *a) {
+    size_t i;
+    div_t dm;
+
+    for (i = 0; i < a->digits_count; i++) {
+        if (a->digits[i] > 9) {
+            if (a->digits_count < i + 1) {
+                a->digits_count = i + 1;
+                a->digits = realloc(a->digits, a->digits_count * sizeof(int));
+                a->digits[a->digits_count - 1] = 0;
+            }
+            dm = div(a->digits[i], 10);
+            a->digits[i] = dm.rem;
+            a->digits[i + 1] += dm.quot;
+        }
+    }
+}
+
+void bignum_string(const bignum *a, char *buf, size_t len) {
+    size_t limit, i;
+
+    limit = (a->digits_count < len ? a->digits_count : len);
+    for (i = 0; i < limit; i++) {
+        buf[i] = a->digits[a->digits_count - i - 1] + '0';
+    }
+    if (limit > 0) {
+        buf[limit] = 0;
+    }
+}
+
+void bignum_mul(bignum *a, const bignum *b) {
+    bignum c;
+    size_t i, j, leading_zeros;
+
+    c.digits_count = a->digits_count + b->digits_count;
+    c.digits = calloc(c.digits_count, sizeof(int));
+    for (i = 0; i < a->digits_count; i++) {
+        for (j = 0; j < b->digits_count; j++) {
+            c.digits[i + j] += a->digits[i] * b->digits[j];
+        }
+    }
+    free(a->digits);
+    a->digits = c.digits;
+    a->digits_count = c.digits_count;
+    bignum_carry(a);
+    leading_zeros = 0;
+    for (i = 0; i < a->digits_count; i++) {
+        if (a->digits[a->digits_count - i - 1] == 0) {
+            leading_zeros++;
+        } else {
+            break;
+        }
+    }
+    a->digits_count -= leading_zeros;
+    a->digits = realloc(a->digits, a->digits_count * sizeof(int));
+}
+
+void bignum_pow(bignum *a, int exp) {
+    bignum *b;
+    int i;
+
+    if (exp == 0) {
+        a->digits_count = 1;
+        a->digits = realloc(a->digits, sizeof(int));
+        a->digits[0] = 1;
+        return;
+    } else if (exp == 1) {
+        return;
+    }
+    b = bignum_copy(a);
+    for (i = 1; i < exp; i++) {
+        bignum_mul(a, b);
+    }
+    bignum_delete(b);
+}
 
 int main(int argc, const char *argv[]) {
-    int a, b, written, i, max_sum, sum;
-    mpz_t rop;
-    char buffer[DIGITS_COUNT_OF_99_99 + 1];
+    bignum *big_a;
+    int max_sum, sum;
+    size_t a, b, i;
 
     max_sum = 0;
-    mpz_init(rop);
-    for (a = 2; a < 100; a++) {
-        for (b = 2; b < 100; b++) {
-            mpz_ui_pow_ui(rop, a, b);
-            written = gmp_snprintf(buffer, DIGITS_COUNT_OF_99_99 + 1, "%Zd", rop);
-            if (written < 0) {
-                goto fail;
-            }
+    for (a = 1; a < 100; a++) {
+        for (b = 1; b < 100; b++) {
+            big_a = bignum_new(a);
+            bignum_pow(big_a, b);
             sum = 0;
-            for (i = 0; i < written; i++) {
-                sum += buffer[i] - '0';
+            for (i = 0; i < big_a->digits_count; i++) {
+                sum += big_a->digits[i];
             }
+            bignum_delete(big_a);
             if (max_sum < sum) {
                 max_sum = sum;
             }
         }
     }
-    mpz_clear(rop);
     printf("%d\n", max_sum);
     return 0;
-
-fail:
-    mpz_clear(rop);
-    return 1;
 }
