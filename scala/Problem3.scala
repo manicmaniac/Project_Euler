1998683aab09daf1cc80382a72d0d1e4e22fe191
Ryosuke Ito <rito.0305@gmail.com>
2020-07-05 15:16:52 +0900

    update

--- scala/Problem3.scala
+++ scala/Problem3.scala
@@ -1,16 +1,14 @@
 /*
-The prime factors of 13195 are 5, 7, 13 and 29.
-What is the largest prime factor of the number 600851475143 ?
-*/
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ * What is the largest prime factor of the number 600851475143 ?
+ */
 
 object Problem3 {
-  def factorize(n:Long, d:Long=2, res:List[Long]=Nil):List[Long] = n match {
-    case `d` => d::res reverse
-    case _ if(n%d==0) => factorize(n/d, d, d::res)
-    case _ => factorize(n, d+1, res)
+  private def factorize(n: Long, d: Long = 2, res: List[Long] = Nil): List[Long] = n match {
+    case `d` => d::res
+    case _ if (n % d == 0) => factorize(n / d, d, d::res)
+    case _ => factorize(n, d + 1, res)
   }
-  def main(args:Array[String]) {
-    println(factorize(600851475143L) max)
-  }
-}
 
+  def main(args: Array[String]) = println(factorize(600851475143L).max)
+}
